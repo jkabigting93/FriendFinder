3b5d97a9ce60fc600d90b0138c538ab3ab7712e4
Joey Kabigting <joey.kabigting@mail.utoronto.ca>
2019-11-03 22:43:35 -0500

    add compatibility logic

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -21,6 +21,22 @@ module.exports = function(app) {
             friendDiff: Infinity
         };
 
+        for(var i = 0; i < friends.length; i++) {
+            var currentFriend = friends[i];
+            scoreDifference = 0;
+        
+            for (var j = 0; j < newUser.scores.length; j++) {
+                friendScore = currentFriend.scores[j];
+                userScore = newUser.scores[j];
+                scoreDifference  += Math.abs(parseInt(userScore) - parseInt(friendScore));
+            } 
+
+            if (scoreDifference <= bestMatch.friendDiff){
+                bestMatch.name =currentFriend.name;
+                bestMatch.photo = currentFriend.photo;
+                bestMatch.friendDiff = scoreDifference ;
+            }
+        } 
         res.json(bestMatch);
     });
 };
