8ee948d22cee12a6e86a6a6b32c11351d296ce80
Joey Kabigting <joey.kabigting@mail.utoronto.ca>
2019-11-06 16:45:27 -0500

    reshuffled array logic to be called on survey.html

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,40 +1,42 @@
-var friends = require("../data/friends");
+var friends = require("../data/friends.js");
+var path = require("path");
 
 module.exports = function(app) {
 
     // GET to display JSON of all possible friends
-    app.get("/api/friends", function(req,res) {
+    app.get("/api/friends", function(req, res) {
         res.json(friends);
     });
     
     // POST that takes in survey results + compatibility logic
-    app.post("/api/friends", function(req,res) {
-        var bestMatch = {
-            name: "",
-            photo: "",
-            friendDifference: 1000
-        };
-        var userData = req.body;
-        var userScores = userData.scores;
-        var userName = userData.name;
-        var userPhoto = userData.photo;
-        var totalDifference = 0;
+    app.post("/api/friends", function(req, res) {
+        friends.push(req.body);
+        // var bestMatch = {
+        //     name: "",
+        //     photo: "",
+        //     friendDifference: 1000
+        // };
+        // var userData = req.body;
+        // var userScores = userData.scores;
+        // var userName = userData.name;
+        // var userPhoto = userData.photo;
+        // var totalDifference = 0;
 
-        // loop through all friends for scores
-        for (var i = 0; i < friends.length - 1; i++) {
-            console.log(friends[i].name);
-            totalDifference = 0;
-            // 2nd loop to compare all other user's scores with current user
-            for (var j = 0; j < 10; j++) {
-                totalDifference += Math.abs(parseInt(userScores[j]) - parseInt(friends[i].scores[j]));
-                if (totalDifference <= bestMatch.friendDifference) {
-                    bestMatch.name = friends[i].name;
-                    bestMatch.photo = friends[i].photo;
-                    bestMatch.friendDifference = totalDifference;
-                }
-            }
-        }
-        friends.push(userData);
-        res.json(bestMatch);
+        // // loop through all friends for scores
+        // for (var i = 0; i < friends.length - 1; i++) {
+        //     console.log(friends[i].name);
+        //     totalDifference = 0;
+        //     // 2nd loop to compare all other user's scores with current user
+        //     for (var j = 0; j < 10; j++) {
+        //         totalDifference += Math.abs(parseInt(userScores[j]) - parseInt(friends[i].scores[j]));
+        //         if (totalDifference <= bestMatch.friendDifference) {
+        //             bestMatch.name = friends[i].name;
+        //             bestMatch.photo = friends[i].photo;
+        //             bestMatch.friendDifference = totalDifference;
+        //         }
+        //     }
+        // }
+        // friends.push(userData);
+        // res.json(bestMatch);
     });
 };
--- server.js
+++ server.js
@@ -7,8 +7,10 @@ var app = express();
 var PORT = process.env.PORT || 3030;
 
 // Sets up the Express app to handle data parsing
-app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({type:"application/vnd.api+json"}));
 
 // Router
 require("./app/routing/apiRoutes")(app);
